61904c4893e4707fceff49ab03656a70ccea1482
njoki254 <njokifaith61@gmail.com>
2020-11-10 13:42:50 +0300

    fix path provider import and directory

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -1,7 +1,8 @@
+import 'dart:io';
+
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'dart:async';
-import 'dart:io';
 import 'data.dart';
 
 class DatabaseHelper{
